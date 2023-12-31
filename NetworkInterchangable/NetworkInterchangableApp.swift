9e83fb7ad6343e55173c0ffad8fdd0e4b0987c06
Muruvvetbati <batimuruvvet@gmail.com>
2023-12-31 14:02:47 +0300

    Initial Commit

---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Devmuruvvet on 31.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
